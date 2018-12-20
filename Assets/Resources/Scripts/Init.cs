31df42ff4c5c136befbfb08d66fcb4930908761f
Maël Nagot <mael.nagot@netent.com>
2018-12-20 23:40:15 +0100

    POC Background picture

---
+++ Assets/Resources/Scripts/Init.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Init : MonoBehaviour {
+
+	private GameObject _backgroundObject;
+	private string _mapConfigurationPath;
+	private string _jsonString;
+
+
+	// Use this for initialization
+	void Start () {
+		_mapConfigurationPath = Application.streamingAssetsPath + "Configurations/mapConfiguration.json";
+		_jsonString = File.ReadAllText(_mapConfigurationPath);
+		ListOfMaps maps = JsonUtility.FromJson<ListOfMaps>(_jsonString);
+		Map initMap = maps.maps[0];
+
+		_backgroundObject = GameObject.Find("Background");
+		_backgroundObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Backgrounds/" + initMap.background);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
