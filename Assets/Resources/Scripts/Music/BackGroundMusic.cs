9283dd55a8d93d4f98729b68f438908c28db98da
Maël Nagot <mael.nagot@netent.com>
2019-01-26 23:10:58 +0100

    Adding Background music controller

---
+++ Assets/Resources/Scripts/Music/BackGroundMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMusic : MonoBehaviour
+{
+    public static BackGroundMusic instance;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        
+        DontDestroyOnLoad(gameObject);
+    }
+}
